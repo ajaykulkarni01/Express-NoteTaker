aba4354543f042792c00e2a1384bfe84645d0c56
Ajay K <ajaycoolkarni@yahoo.com>
2021-11-20 17:51:11 +1030

    middleware, routes to api added

--- server.js
+++ server.js
@@ -1,18 +1,40 @@
 // Dependencies
 
-var express = require("express");
-var path = require("path");
-const fs = require("fs");
-const exp = require("constants");
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index.js');
+
 
 // Sets up and initializes the Express App server
-var app = express();
+const app = express();
 // Sets an initial port, use later in listener
-var PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
+
+// listen for requests
+app.listen(PORT, () => {
+    console.log(`Application listening at http://localhost:${PORT}`);
+});
+
+// serve up the static pages in the public folder
+app.use(express.static('public'));
+
 
 // Middleware sets up the Express app to find/use files and handle data parsing
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'public')));
+// route to api
+app.use('/api', api);
 
+// GET Route for homepage
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for notes
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// wildcard redirect to homepage
+app.get('*', (req, res) => res.redirect('/'));
